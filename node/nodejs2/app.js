f756d30d5918913c7b6f587cc2ed39b9e8010806
edijav85 <jav.11021jepnpm@gmail.com>
2016-12-14 16:50:13 -0500

    creacio de npms

---
+++ node/nodejs2/app.js
@@ -0,0 +1,13 @@
+//tecnologiaswebqbelen.imprimirSuma1y2
+//
+module.exports ={
+    imprimirSuma1y2: imprimirSuma1y2,
+}
+
+function imprimirSuma1y2(){
+    var numero1 = 1,
+    numero2 = 2;
+
+console.log(numero1 + numero2);
+    
+}
